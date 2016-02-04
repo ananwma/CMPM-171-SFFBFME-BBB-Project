f834f91c63d4d44144bbec9c50dcc565882653ff
rlmurray <rlmurray@ucsc.edu>
2016-02-03 19:27:04 -0800

    Update and rename character.cpp to Character.cpp

---
+++ SFF/Character.cpp
@@ -0,0 +1,19 @@
+#include "Character.h"
+
+
+
+Character::Character() {
+
+}
+
+Character::Character(vector <Move> moveList_n, string spritesheet_url)
+{
+	moveList = moveList_n;
+	spritesheet = spritesheet_url;
+}
+
+
+
+Character::~Character()
+{
+}
