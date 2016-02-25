2d6708eb27956ba8e548fb7e6ec5881539ba3542
mhgunnin <mhgunnin@ucsc.edu>
2016-02-24 20:03:16 -0800

    files

--- BachStrong.h
+++ BachStrong.h
@@ -14,7 +14,7 @@ BachStrong::BachStrong() {
 		exit(EXIT_FAILURE);
 	}
 	// Number of animation frames
-	frameCount = 3;
+	frameCount = 7;
 	// Damage
 	damage = 50;
 	// Change in position on move
@@ -34,16 +34,41 @@ void BachStrong::initFrames() {
 	hit.clear();
 	hurt.clear();
 	/***FRAME 2***/
+	hurt.push_back(sf::FloatRect(139, 289, 231, 239));
+	hurt.push_back(sf::FloatRect(125, 3, 213, 292));
+	frameVector.push_back(Frame(hit, hurt));
+	hit.clear();
+	hurt.clear();
+	/***FRAME 3***/
 	hurt.push_back(sf::FloatRect(108, 296, 268, 223));
 	hurt.push_back(sf::FloatRect(114, 9, 247, 288));
 	frameVector.push_back(Frame(hit, hurt));
 	hit.clear();
 	hurt.clear();
-	/***FRAME 3***/
+	/***FRAME 4***/
+	hurt.push_back(sf::FloatRect(108, 296, 268, 223));
+	hurt.push_back(sf::FloatRect(114, 9, 247, 288));
+	frameVector.push_back(Frame(hit, hurt));
+	hit.clear();
+	hurt.clear();
+	/***FRAME 5***/
 	hurt.push_back(sf::FloatRect(177, 55, 210, 227));
 	hit.push_back(sf::FloatRect(162, 75, 293, 114));
 	hurt.push_back(sf::FloatRect(110, 277, 274, 264));
 	hurt.push_back(sf::FloatRect(110, 52, 174, 216));
+	frameVector.push_back(Frame(hit, hurt));
+	hit.clear();
+	hurt.clear();
+	/***FRAME 6***/
+	hurt.push_back(sf::FloatRect(177, 55, 210, 227));
+	hurt.push_back(sf::FloatRect(110, 277, 274, 264));
+	hurt.push_back(sf::FloatRect(110, 52, 174, 216));
+	frameVector.push_back(Frame(hit, hurt));
+	hit.clear();
+	hurt.clear();
+	/***FRAME 7***/
+	hurt.push_back(sf::FloatRect(108, 296, 268, 223));
+	hurt.push_back(sf::FloatRect(114, 9, 247, 288));
 	frameVector.push_back(Frame(hit, hurt));
 	hit.clear();
 	hurt.clear();
