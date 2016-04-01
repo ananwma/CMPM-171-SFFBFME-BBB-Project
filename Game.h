039fae867089979422bd71770615abdae18b2a48
mhgunnin <mhgunnin@ucsc.edu>
2016-03-31 18:33:54 -0700

    some stuff still broken

--- Game.h
+++ Game.h
@@ -7,9 +7,10 @@
 #include "AssetManager.h"
 #include <SFML/Graphics.hpp>
 
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
 #define BEAT_SPEED 500.0f
+#define GROUND WINDOW_WIDTH / 4
 
 // c
 
