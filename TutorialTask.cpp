8de6e1baa8c76403820389931994b94a22389220
Ronald Murray <rlmurray@ucsc.edu>
2016-04-17 19:24:09 -0700

    Tutorial Classes. Nothing working yet

---
+++ TutorialTask.cpp
@@ -0,0 +1,15 @@
+#include "TutorialTask.h"
+
+TutorialTask::TutorialTask()
+{
+
+}
+
+TutorialTask::TutorialTask(bool tskch(), string tsk)
+{
+	taskComplete = false;
+	testTask = tskch;
+	task = tsk;
+	
+}
+
---
+++ TutorialTask.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TutorialTask {
+public:
+	bool taskComplete;
+	bool testTask();
+	TutorialTask();
+	TutorialTask(bool(), string);
+	string task;
+};
